4a4c22d6d2872362d0e9af74e5c62ede8a503d50
solsticebloom <solsticebloom179@gmail.com>
2024-08-28 15:17:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Document loaded and ready!');
 });
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
