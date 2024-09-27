0eb981eb306342e486c2d02fb4e94405c03961d1
solsticebloom <solsticebloom179@gmail.com>
2024-09-27 22:02:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// JavaScript for menu toggle
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  alert('Menu clicked! You can add more functionality here.');
-});
-
-// JavaScript for event interactions
-document.querySelector('.event-booking').addEventListener('click', () => {
-  alert('Booking functionality coming soon!');
-});
-
-document.querySelector('.more-about-event').addEventListener('click', () => {
-  alert('Redirecting to more event details...');
-});
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
